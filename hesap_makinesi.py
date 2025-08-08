b60cd9dde807ab1bdc62a4758f7c9537e7a28d4f
Yusufztrk03 <yusuf170071@gmail.com>
2025-08-08 14:26:59 +0300

    Add files via upload

---
+++ hesap_makinesi.py
@@ -0,0 +1,25 @@
+ print('Hesap Makinesi')
+sayı1 = float(input('Birinci sayıyı giriniz:'))
+sayı2 = float(input('İkinci sayıyı giriniz:'))
+
+print('İşlemler:')
+print('1. Toplama')
+print('2. Çıkarma')
+print('3. Çarpma')
+print('4. Bölme')
+
+seçim = input('Seçiminizi yapınız: (1/2/3/4:)')
+
+if seçim =='1':
+    print('sonuç:', sayı1 + sayı2)
+elif seçim =='2':
+    print('sonuç:', sayı1 - sayı2)
+elif seçim =='3':
+    print('sonuç:', sayı1 * sayı2)
+elif seçim =='4':
+    if sayı2 != 0:
+        print('sonuç', sayı1 / sayı2)
+    else:
+        print('Hata: Sıfırla bölme yapılmaz!')
+else:
+    print('Geçersiz seçim!')
